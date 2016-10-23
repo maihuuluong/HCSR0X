dd2d618edce4e9d00d942384813c8c018cbfe733
maihuuluong <maihuuluong@gmail.com>
2016-10-23 14:28:22 +0700

    initially add

---
+++ HCSR0X.cpp
@@ -0,0 +1,58 @@
+/*
+  HCSR0X.cpp
+  Mai Huu Luong - Nov, 2015
+*/
+
+#include  <Arduino.h>
+#include "HCSR0X.h"
+
+
+HCSR0X::HCSR0X( uint8_t Trigger, uint8_t Echo ,  int max_range_in_cm){
+
+
+
+  pinMode(Trigger, OUTPUT);
+  pinMode(Echo, INPUT);
+
+  _TriggerBit = digitalPinToBitMask(Trigger);
+  _EchoBit = digitalPinToBitMask(Echo);
+
+  _TriggerRegister = portOutputRegister(digitalPinToPort(Trigger));
+  _EchoRegister   = portInputRegister(digitalPinToPort(Echo));
+  _Time_out = max_range_in_cm * SONARS_SPEED_RATIO_CM << 1;
+}
+
+float  HCSR0X::ping(int type)
+{
+
+  long BeginChrono,EndChrono,DeltaChrono =0;
+
+  BeginChrono  = micros();
+
+  BIT_OFF(_TriggerRegister, _TriggerBit);
+  _delay_us(2);
+  //digitalWrite(_Trigger, HIGH);
+  BIT_ON(_TriggerRegister, _TriggerBit);
+  //_delay_10_micros();
+  _delay_us(10);
+  //digitalWrite(_Trigger, LOW);
+  BIT_OFF(_TriggerRegister , _TriggerBit);
+
+  _break_time = micros() + _Time_out;
+  while(!BIT_GET(_EchoRegister,_EchoBit)){
+    if (micros() > _break_time)
+      return ERROR_RANGE;
+  }
+  BeginChrono  = micros();
+  _break_time  = BeginChrono + _Time_out;
+
+  while(BIT_GET(_EchoRegister,_EchoBit)) { 			//Echo begin wait until echo response end
+    EndChrono = micros();
+
+    if (EndChrono > _break_time)
+      return OUT_RANGE;
+  }
+  DeltaChrono = EndChrono - BeginChrono - 20; // overheat approximately 20 microseconds for the operations.
+  return float(DeltaChrono/2)/(type == CM?SONARS_SPEED_RATIO_CM:SONARS_SPEED_RATIO_INC);
+}
+
---
+++ HCSR0X.h
@@ -0,0 +1,85 @@
+/*
+  HCSR0X.h - Library  for sensor HCSR0X
+*/
+
+#ifndef HCSR0X_h
+#define HCSR0X_h
+
+#include <Arduino.h>
+
+
+#define CM 1
+#define IN 0
+
+#define ERROR_RANGE -2
+#define OUT_RANGE -1
+
+#define BIT_ON(REG, BIT) *REG|=BIT
+#define BIT_OFF(REG, BIT) *REG&=~BIT
+#define BIT_GET(REG, BIT)  (*REG&BIT?HIGH:LOW)
+
+#define DEFAULT_RANGE       500 // default max range in centimeters
+#define SONARS_SPEED_RATIO_CM  29
+#define SONARS_SPEED_RATIO_INC  74
+
+
+
+#define PIN_INPUT(REG, BIT) {uint8_t oldSREG = SREG;\
+                            cli();\
+                            *REG &= ~BIT;\
+                            SREG = oldSREG;\
+                            }
+#define PIN_OUTPUT(REG, BIT) {int8_t oldSREG = SREG;\
+                             cli();\
+                             *REG |= BIT;\
+                             SREG = oldSREG;\
+                             }
+
+
+
+#define _delay_10_micros() __asm__("    ldi  r18, 53\n\t\
+                                        L1: dec  r18\n\t\
+                                        brne L1\n\t\
+                                        nop\n\t\
+                                  ")
+
+
+
+class HCSR0X
+{
+  public:
+  HCSR0X(uint8_t Trigger,uint8_t Echo, int max_range_in_cm = DEFAULT_RANGE);
+
+   /*!
+    @method
+    @abstract   Sets all the pins of the device.
+    @discussion This method sets all the pins of the device Trigger
+    and Echo.
+
+    @param       Trigger[in] device pin to send pulses. Range (0..12).
+    @param       Echo[out] to set the pin of answer device (0..12).
+    @param       Time_out max waiting time in milliseconds.
+    */
+
+  float ping(int type = CM);
+
+   /*!
+    @method
+    @abstract   Starts calculating a distance.
+    @discussion This method use this Formula: delay in uS / 58 = centimeters.
+
+    @param      type[integer] centimeters or inch CM/INC
+    @result     distance in centimeters or inch
+    */
+
+  private:
+    long _Time_out;
+    volatile uint8_t * _TriggerRegister;
+    volatile uint8_t * _EchoRegister;
+    uint8_t _TriggerBit;
+    uint8_t _EchoBit;
+    long _break_time;
+};
+
+#endif
+
